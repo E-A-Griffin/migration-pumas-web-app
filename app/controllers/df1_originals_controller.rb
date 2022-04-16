d5c1a355618f768ab990dbc0b26dd1cc1960a2a8
E-A-Griffin <eagriffin@usf.edu>
2022-04-16 15:23:41 -0400

    Add support for top-k outflows, general to/from city/metro queries, and animation toggling. Change synchronous page updates to asynchronous

--- app/controllers/df1_originals_controller.rb
+++ app/controllers/df1_originals_controller.rb
@@ -1,34 +1,6 @@
-class Df1OriginalsController < ApplicationController
-  before_action :set_df1_original, only: %i[ show edit update destroy ]
-
+module City
   # Constants
-  # @@METRO_SQL = "with o_states_and_pumas as (
-  #                  select distinct fips_state_code, pw_puma00_migpuma1
-  #                  from msa_delineation_2018, census_puma_relation,
-  #                       puma_to_powpuma_relation
-  #                  where CSA_Code = ? and
-  #                        st = fips_state_code and
-  #                        county = fips_county_code and
-  #                        puma_to_powpuma_relation.puma = puma_5ce),
-  #                  d_states_and_pumas as (
-  #                    select distinct fips_state_code, pw_puma00_migpuma1
-  #                    from msa_delineation_2018, census_puma_relation,
-  #                         puma_to_powpuma_relation
-  #                    where CSA_Code = ? and
-  #                          st = fips_state_code and
-  #                          county = fips_county_code and
-  #                          puma_to_powpuma_relation.puma = puma_5ce)
-
-  #                select substr(serial_no, 1, 4) as year, age, person_number,
-  #                       person_weight, state as destination_state,
-  #                       state_o as original_state, migpuma_o as original_puma,
-  #                       migpuma_d as destination_puma
-  #                from df1
-  #                where (state_o, migpuma_o) in
-  #                      (select * from o_states_and_pumas) and
-  #                      (state, migpuma_d) in
-  #                      (select * from d_states_and_pumas);"
-  @@METRO_SQL = "with o_states_and_pumas as (
+  @@METRO_SQL_ORIGIN_PREFIX = "with o_states_and_pumas as (
                    select distinct fips_state_code, migpuma10
                    from msa_delineation_2018, census_puma_relation,
                         crosswalk_2000_2010, puma_to_powpuma_relation
@@ -36,7 +8,9 @@ class Df1OriginalsController < ApplicationController
                          st = fips_state_code and
                          county = fips_county_code and
                          migpuma10 = pw_puma00_migpuma1 and
-                         puma = puma_5ce),
+                         puma = puma_5ce),"
+
+  @@METRO_SQL_DEST_PREFIX = "
                    d_states_and_pumas as (
                      select distinct fips_state_code, migpuma10
                      from msa_delineation_2018, census_puma_relation,
@@ -45,9 +19,43 @@ class Df1OriginalsController < ApplicationController
                            st = fips_state_code and
                            county = fips_county_code and
                            migpuma10 = pw_puma00_migpuma1 and
-                           puma = puma_5ce)
+                           puma = puma_5ce)"
 
-                 select substr(serial_no, 1, 4) as year, age, person_number,
+ @@CITY_SQL_ORIGIN_PREFIX = "
+   with o_states_and_pumas as (
+                   select distinct fips_state_code, migpuma10
+                   from msa_delineation_2018, census_puma_relation,
+                        crosswalk_2000_2010, puma_to_powpuma_relation
+                   where cbsa_title = ? and
+                         st = fips_state_code and
+                         county = fips_county_code and
+                         migpuma10 = pw_puma00_migpuma1 and
+                         puma = puma_5ce),"
+
+ @@CITY_TOP_K_OUTFLOW_PREFIX = "
+     -- mig_o_states_and_pumas necessary due to issue of 2000 vs. 2010
+     -- representation of puma codes
+     mig_o_states_and_pumas as (
+                   select distinct state_o,
+                                   puma_to_powpuma_relation.puma
+                   from df1, puma_to_powpuma_relation
+                   where (state_o, migpuma_o) in
+                         (select * from o_states_and_pumas) and
+                         migpuma_o = pw_puma00_migpuma1     and
+                         state_o   = res_state)"
+
+ @@CITY_SQL_DEST_PREFIX = "
+                   d_states_and_pumas as (
+                     select distinct fips_state_code, migpuma10
+                     from msa_delineation_2018, census_puma_relation,
+                          crosswalk_2000_2010, puma_to_powpuma_relation
+                     where cbsa_title = ? and
+                           st = fips_state_code and
+                           county = fips_county_code and
+                           migpuma10 = pw_puma00_migpuma1 and
+                           puma = puma_5ce)"
+
+ @@CITY_SQL_SUFFIX =  " select substr(serial_no, 1, 4) as year, age, person_number,
                         person_weight, state::text as destination_state,
                         state_o::text as original_state,
                         puma_to_powpuma_relation.puma as original_puma,
@@ -58,18 +66,87 @@ class Df1OriginalsController < ApplicationController
                        (state, migpuma_d) in
                        (select * from d_states_and_pumas) and
                        migpuma_o = pw_puma00_migpuma1     and
-                       state_o   = res_state;"
+                       state_o   = res_state"
+
+  @@CITY_TOP_K_OUTFLOW_SUFFIX = "
+  select sum(person_weight) as population,
+         min(state_o)::text as original_state,
+         min(puma_to_powpuma_relation.puma)::int as original_puma,
+         state::text as destination_state,
+         df1.puma::int as destination_puma
+   from df1, puma_to_powpuma_relation
+   where (state_o, migpuma_o) in (select * from o_states_and_pumas)  and
+         (state::int, df1.puma::int) not in (select * from mig_o_states_and_pumas) and
+         migpuma_o = pw_puma00_migpuma1                              and
+         state_o   = res_state                                       and
+         substr(serial_no, 1, 4)::int between ? and ?
+   group by destination_puma, destination_state
+   order by sum(person_weight) desc
+   limit ?;"
+
+  @@CITY_SQL_YEAR_CLAUSE = " and substr(serial_no, 1, 4) = ?;"
+
+  def is_number? string
+    true if Integer(string) rescue false
+  end
+
+
+  def get_proper_sql_arr(o_city, d_city, k, start_year, end_year)
+    # if the user supplied an o_city and not a d_city then we need to select
+    # the top k outflows
+    #
+    # top k outflows from o_city
+    if !o_city.nil? and d_city.nil?
+      o_city_name_or_code = Df1Original.city_name_to_CSA(o_city)
+      o_city_prefix = is_number?(o_city_name_or_code) ?
+                      @@METRO_SQL_ORIGIN_PREFIX : @@CITY_SQL_ORIGIN_PREFIX
+      mig_o_city_prefix = @@CITY_TOP_K_OUTFLOW_PREFIX
+      suffix = @@CITY_TOP_K_OUTFLOW_SUFFIX
+      [o_city_prefix + mig_o_city_prefix + suffix,
+       o_city_name_or_code, start_year.to_i,
+       end_year.to_i, k.to_i]
+
+    # top k inflows from d_city is invalid as information from database cannot
+    # be extrapolated
+    elsif o_city.nil? and !d_city.nil?
+      STDERR.puts("Non-nil d_city provided with nil o_city. Top k inflows " +
+                  "are not supported by application and should be prevented " +
+                  "from requesting on the front end")
+    # o_city->d_city flows
+    else
+      o_city_name_or_code = Df1Original.city_name_to_CSA(o_city)
+      d_city_name_or_code = Df1Original.city_name_to_CSA(d_city)
+      o_city_prefix = is_number?(o_city_name_or_code) ?
+                      @@METRO_SQL_ORIGIN_PREFIX : @@CITY_SQL_ORIGIN_PREFIX
+      d_city_prefix = is_number?(d_city_name_or_code) ?
+                      @@METRO_SQL_DEST_PREFIX : @@CITY_SQL_DEST_PREFIX
+      if (Df1Original.year?(year))
+        suffix = @@CITY_SQL_SUFFIX + @@CITY_SQL_YEAR_CLAUSE
+        [o_city_prefix + d_city_prefix + suffix, o_city_name_or_code,
+         d_city_name_or_code, year]
+      else
+        suffix = @@CITY_SQL_SUFFIX + ";"
+        [o_city_prefix + d_city_prefix + suffix, o_city_name_or_code,
+         d_city_name_or_code]
+      end
+    end
+  end
+end
+
+class Df1OriginalsController < ApplicationController
+  before_action :set_df1_original, only: %i[ show edit update destroy ]
+  include City
 
   # GET /df1_originals or /df1_originals.json
   def index
     @query = params
-    if params[:o_city] and params[:d_city]
+    # params[:year] is optional, so don't check for it until forming SQL
+    if params[:o_city] and params[:k] and params[:start_year] and
+       params[:end_year]
       clean_sql =
-        ActiveRecord::Base::sanitize_sql_array([@@METRO_SQL,
-                                                Df1Original.metro_name_to_CSA(
-                                                  params[:o_city]),
-                                                Df1Original.metro_name_to_CSA(
-                                                  params[:d_city])])
+        ActiveRecord::Base::sanitize_sql_array(
+          get_proper_sql_arr(params[:o_city], params[:d_city], params[:k],
+                             params[:start_year], params[:end_year]))
       record_results = ActiveRecord::Base.connection.execute(clean_sql)
 
       @df1_originals = record_results
@@ -77,7 +154,8 @@ class Df1OriginalsController < ApplicationController
       # Get only three element so computer doesn't crash
       @df1_originals = Df1Original.take(3)
     end
-    @metro = Df1Original.get_top_10_metro
+    @all_cities = Df1Original.get_all_cities
+    @years = Df1Original.get_years
 
     # Lambda function to process csv based on attributes of SQL results
     to_csv = -> (records) {
@@ -94,7 +172,6 @@ class Df1OriginalsController < ApplicationController
           csv << attributes
 
           records.each do |row|
-            puts row.attributes.values_at(*attributes)
             csv << row.attributes.values_at(*attributes)
           end
         end
@@ -102,17 +179,20 @@ class Df1OriginalsController < ApplicationController
     }
     respond_to do |format|
       format.html
-      format.csv { send_data to_csv.call(@df1_originals),
+      format.csv { send_data to_csv.call(record_results),
                              :filename =>
                              (@query.key?(:o_city) and
-                             @query.key?(:d_city) ?
-                             'from-' +
-                             @query[:o_city].tr(' ', '_') +
-                             '-to-' +
-                             @query[:d_city].tr(' ', '_') +
-                             '.csv' :
-                               'df1_originals.csv')
+                              @query.key?(:d_city) ?
+                              'from-' +
+                              @query[:o_city].tr(' ', '_') +
+                              '-to-' +
+                              @query[:d_city].tr(' ', '_') +
+                              (@query[:year].empty? ? '' :
+                                                      '-' + @query[:year]) +
+                              '.csv' :
+                              'df1_originals.csv')
       }
+      format.json { render json: @df1_originals }
     end
   end
 
@@ -135,11 +215,14 @@ class Df1OriginalsController < ApplicationController
 
     respond_to do |format|
       if @df1_original.save
-        format.html { redirect_to df1_original_url(@df1_original), notice: "Df1 original was successfully created." }
+        format.html { redirect_to df1_original_url(@df1_original),
+                                  notice:
+                                    "Df1 original was successfully created." }
         format.json { render :show, status: :created, location: @df1_original }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @df1_original.errors, status: :unprocessable_entity }
+        format.json { render json: @df1_original.errors,
+                             status: :unprocessable_entity }
       end
     end
   end
@@ -148,11 +231,14 @@ class Df1OriginalsController < ApplicationController
   def update
     respond_to do |format|
       if @df1_original.update(df1_original_params)
-        format.html { redirect_to df1_original_url(@df1_original), notice: "Df1 original was successfully updated." }
+        format.html { redirect_to df1_original_url(@df1_original),
+                                  notice:
+                                    "Df1 original was successfully updated." }
         format.json { render :show, status: :ok, location: @df1_original }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @df1_original.errors, status: :unprocessable_entity }
+        format.json { render json: @df1_original.errors,
+                             status: :unprocessable_entity }
       end
     end
   end
@@ -162,7 +248,9 @@ class Df1OriginalsController < ApplicationController
     @df1_original.destroy
 
     respond_to do |format|
-      format.html { redirect_to df1_originals_url, notice: "Df1 original was successfully destroyed." }
+      format.html { redirect_to df1_originals_url,
+                                notice:
+                                  "Df1 original was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -175,6 +263,7 @@ class Df1OriginalsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def df1_original_params
-      params.require(:df1_original).permit(:record_type, :serial_no, :city)
+      params.require(:df1_original).permit(:record_type, :serial_no, :city,
+                                           :year)
     end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,6 +6,10 @@ import "controllers"
 // Leaflet map state
 let map_state = {'map': {}, 'tracks': {}, 'connections': []};
 
+// Takes a function and an initial argument and returns a new function based on
+// the function argument being supplied its initial argument
+const partial = (fn, ...first_args) => (...args) => fn(...first_args, ...args);
+
 // Takes an array (presumably of Objects) and returns an array with duplicates
 // removed
 const get_deduped_arr =
@@ -41,31 +45,87 @@ const turn_animations_off = (map_state) =>
 const turn_animations_on = (map_state) =>
       map_state['connections'].forEach((conn) => conn.playAnimation());
 
+// Toggle whether animations play based on current state of animations button
 const toggle_animations_button = (btn) => {
     let btn_element = btn.currentTarget;
-    let parent = btn_element.parentElement;
-    if (btn_element.classList.contains('enabled')) {
-        btn_element.classList.replace('enabled', 'disabled');
-        btn_element.firstElementChild.innerText = 'Enable Animations';
-        parent.children[1].innerText = 'Animations Currently Disabled';
+    if (!btn_element.checked)
         turn_animations_off(map_state);
-    }
-    else if (btn_element.classList.contains('disabled')) {
-        btn_element.classList.replace('disabled', 'enabled');
-        btn_element.firstElementChild.innerText = 'Disable Animations';
-        parent.children[1].innerText = 'Animations Currently Enabled';
+    else if (btn_element.checked)
         turn_animations_on(map_state);
-    }
     else
-        console.log("Error toggling animations, wrong inner button format");
+        console.log("Error toggling animations, wrong checkbox format");
 }
 
-$('#animation-btn-id')[0].addEventListener("click", toggle_animations_button);
+// Takes in a string of a form_id and returns the values of the respective form
+// matching form_id, represented as a string of query parameters
+const form_vals_to_q_params = (form_id) => {
+    const form_elements = $('#' + form_id).serializeArray();
+    const param_object =
+          form_elements.reduce((m, obj) => {
+              if(obj['value'] !== '') m[obj['name']] = obj['value'];
+              return m;}, {});
 
-const DEFAULT_O_CITY_TEXT = 'Select origin city';
-const DEFAULT_D_CITY_TEXT = 'Select destination city';
-let o_city_selected = is_city_selected('#o_city', DEFAULT_O_CITY_TEXT);
-let d_city_selected = is_city_selected('#d_city', DEFAULT_D_CITY_TEXT);
+    return $.param(param_object);
+}
+
+const draw_new_conns = async (json, are_animations_on) => {
+    const results_arr = get_deduped_arr(json);
+    const subset_arr = results_arr.slice(0,10);
+    const subset_len = subset_arr.length;
+    subset_arr.map((arr, index) => {
+        console.log([arr['destination-state'],
+                     arr['destination-puma'],
+                     arr['original-state'],
+                     arr['original-puma']]);
+        setTimeout(() => {
+            map_state['connections'].push(
+                draw_connection(map_state['map'], map_state['tracks'],
+                                arr['destination-state'],
+                                arr['destination-puma'],
+                                arr['original-state'],
+                                arr['original-puma']));
+            if (are_animations_on)
+                turn_animations_on(map_state);
+        }, 50 + index*2);}); // arbitrary increasing delay rate to keep web app
+                             // from crashing
+}
+
+// Takes in a set of expec
+const has_schema = (schema_keys_set, json_element) => {
+    const ks = Object.keys(json_element);
+    return ks.length === schema_keys_set.size &&
+           !ks.map((k) => schema_keys_set.has(k)).some((bool) => !bool);}
+
+const has_outflow_schema = partial(has_schema, new Set(['population',
+                                                        'destination_state',
+                                                        'destination_puma']));
+
+// Function used by search form to asynchronously fetch the results of the given
+// query from the backend
+const fetch_search_results = async (e) => {
+    e.preventDefault();
+    const q_params = form_vals_to_q_params('metro-search-id');
+    const url = e.target.action + '?' + q_params;
+
+    return fetch(url, {
+      headers : {'Accept': 'application/json'}
+    }).then((response) => response.json())
+      .then((json) => {
+          let animations_button = $('#animation-btn-id')[0];
+          // Case 1: Top k outflows
+          if (has_outflow_schema(json[0])) {
+              console.log(json);
+              draw_new_conns(json, animations_button.checked);
+          }
+          // Case 2: o_city->d_city flow
+          else {
+              console.log(json);
+              draw_new_conns(json, animations_button.checked);}});
+}
+
+$('#animation-btn-id')[0].addEventListener("change", toggle_animations_button);
+$('#metro-search-id').submit(fetch_search_results);
+
 // Workaround for issue of including download button in rails generated form
 let metro_form = document.getElementById("metro-search-id");
 let dwnld_col  = document.getElementById("download-col-id");
@@ -87,34 +147,40 @@ const async_init_leaflet = async (map_state) => {
     return map_state;
 }
 
+// Dynamically filter dropdown list based on input value
+const filter_end_years = (start_year_select, end_year_select) => {
+    const start_year = parseInt(start_year_select.value);
+    let options = end_year_select.getElementsByTagName("option");
+    console.log("options " + options);
+    Array.from(options).forEach((op) => {
+        if (parseInt(op.value) < start_year)
+            op.setAttribute("disabled", "");
+        else
+            op.removeAttribute("disabled");});
+}
 
-// Enable buttons only when applicable
-$('#o_city').on('change',
-                () => {
-                    o_city_selected = is_city_selected('#o_city',
-                                                       DEFAULT_O_CITY_TEXT);
-                    update_disabled(o_city_selected, d_city_selected);
-                });
+// Set lower bound of end year to value of start year. If selected end year is
+// less than selected start year than automatically set end year value to lower
+// bound
+const ensure_start_year_not_after_end = (end_year_select, e) => {
+    const start_year_select = e.currentTarget;
+    const start_year = parseInt(e.currentTarget.value);
+    const end_year = parseInt(end_year_select.value);
+    if (end_year < start_year)
+        end_year_select.value = e.currentTarget.value;
+    filter_end_years(start_year_select, end_year_select);
+};
 
-$('#d_city').on('change',
-                () => {
-                    d_city_selected = is_city_selected('#d_city',
-                                                       DEFAULT_D_CITY_TEXT);
-                    update_disabled(o_city_selected, d_city_selected);
-                });
+let end_year_select = $('#end_year')[0];
 
-
+$('#start_year').on('change', partial(ensure_start_year_not_after_end,
+                                      end_year_select));
 map_state = await async_init_leaflet(map_state);
 
 
 // Check for search results to be added to
 const results_html = $('#results-hash')[0];
 
-
-// FIXME: Certain layers returned by get_track are nested and so don't have
-// a getCenter function implemented. Solution is probably to calculate
-// center of childLayers on the fly, or to simply choose one layer's center
-// arbitrarily
 if (results_html) {
     const results_arr = get_deduped_arr(JSON.parse(results_html.innerHTML));
     results_arr.slice(0,1).map((arr, index) => {
@@ -133,5 +199,93 @@ if (results_html) {
                    50 + index*2);});
     console.log(map_state);
 }
-else
-    console.log("not good");
+
+
+let dropdown_origin_div = $("#dropdown-origin-div-id")[0];
+let dropdown_origin_search = $("#dropdown-origin-search-id")[0];
+let dropdown_destination_div = $("#dropdown-destination-div-id")[0];
+let dropdown_destination_search = $("#dropdown-destination-search-id")[0];
+
+// When the user clicks on the button,
+// toggle between hiding and showing the dropdown content */
+const toggle_search_fields_visibility = (dropdown_div) =>
+        dropdown_div.classList.add("show");
+
+// Dynamically filter dropdown list based on input value
+const filter_search_fields = (dropdown_search, dropdown_div) => {
+    const filter = dropdown_search.value.toUpperCase();
+    let a = dropdown_div.getElementsByTagName("a");
+    Array.from(a).forEach((e) => {
+        const txt_val = e.txtContent || e.innerText;
+        e.style.display =
+            txt_val.toUpperCase().indexOf(filter) > -1 ? "" : "none";});
+}
+
+// When the user clicks outside the input, remove visibility for the associated
+// dropdown filter selection
+document.addEventListener('click', (e) => {
+  const is_click_inside_origin =
+        dropdown_origin_search.contains(e.target);
+  const is_click_inside_destination =
+        dropdown_destination_search.contains(e.target);
+  if (!is_click_inside_origin)
+    dropdown_origin_div.classList.remove('show');
+  if (!is_click_inside_destination)
+    dropdown_destination_div.classList.remove('show');
+});
+
+// When input changes, update list of metro cities that match input value
+// dynamically
+dropdown_origin_search.addEventListener('keyup', partial(filter_search_fields,
+                                                         dropdown_origin_search,
+                                                         dropdown_origin_div));
+dropdown_destination_search.addEventListener('keyup',
+                                             partial(filter_search_fields,
+                                                     dropdown_destination_search,
+                                                     dropdown_destination_div));
+
+// From clicked a tag, set associated parent's input value and 'selected'
+// attribute
+const set_select_based_on_a_tag = (a, e) => {
+    console.log('click');
+    let content = a.txtContent || a.innerText;
+    let relevant_parent =
+        e.target.parentElement.parentElement.firstElementChild;
+
+    relevant_parent.value = content;
+    relevant_parent.setAttribute("selected", content);
+
+    update_disabled(true, true);
+}
+
+// Add eventlisteners for every a tag representing each city/metro area
+[...dropdown_origin_div.children, ...dropdown_destination_div.children]
+     .forEach((a) =>
+              a.addEventListener('click',
+                                 partial(set_select_based_on_a_tag, a)));
+
+// When input is clicked/selected, populate visible dropdown options for cities
+dropdown_origin_search.addEventListener('click',
+                                        partial(toggle_search_fields_visibility,
+                                                dropdown_origin_div));
+dropdown_destination_search
+    .addEventListener('click',
+                      partial(toggle_search_fields_visibility,
+                              dropdown_destination_div));
+
+// When input changes, check to see if value matches 'selected' attribute, and,
+// if not, then reset 'selected' attribute and disable search button
+[dropdown_origin_search, dropdown_destination_search]
+    .map((d) =>
+         d.addEventListener('change',
+                            (e) => {
+                                let relevant_parent =
+                                    e.target
+                                     .parentElement
+                                     .firstElementChild;
+                                if (relevant_parent.value !=
+                                    relevant_parent.getAttribute("selected")) {
+                                    relevant_parent.setAttribute("selected",
+                                                                 "");
+
+                                    update_disabled(false, false);}}));
